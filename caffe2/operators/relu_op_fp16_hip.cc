e9ec904c6f9afd036578a03dcb2e4d6f4c80ee9b
P. Yeh <pyeh@amd.com>
2018-03-01 21:39:13 -0800

    fix relu op for fp16

--- caffe2/operators/relu_op_fp16_hip.cc
+++ caffe2/operators/relu_op_fp16_hip.cc
@@ -16,46 +16,30 @@
  */
 
 #include "caffe2/core/common_hip.h"
-#ifdef CAFFE_HAS_CUDA_FP16
-
 #include "caffe2/core/context_hip.h"
 #include "caffe2/operators/relu_op.h"
 
 namespace caffe2 {
 namespace {
-__global__ void ReluKernelHalf(const int N, const half* X, half* Y) {
-  const half kZero = __float2half(0.0);
+__global__ void ReluKernelHalf(const int N, const __half* X, __half* Y) {
+  const __half kZero = __float2half(0.0);
   HIP_1D_KERNEL_LOOP(i, N) {
-#if __CUDA_ARCH__ >= 530
     Y[i] = __hgt(X[i], kZero) ? X[i] : kZero;
-#else
-    Y[i] = (__half2float(X[i]) > 0) ? X[i] : kZero;
-#endif
   }
 }
 
-__global__ void ReluKernelHalf2(const int N, const half2* X, half2* Y) {
-  const half2 kZero = __float2half2_rn(0.0);
+__global__ void ReluKernelHalf2(const int N, const __half2* X, __half2* Y) {
+  const __half2 kZero = __float2half2_rn(0.0);
   HIP_1D_KERNEL_LOOP(i, N) {
-#if __CUDA_ARCH__ >= 530
     Y[i] = __hmul2(__hgt2(X[i], kZero), X[i]);
-#else
-    float2 xx = __half22float2(X[i]);
-    Y[i] = __floats2half2_rn(xx.x > 0 ? xx.x : 0.f,
-                             xx.y > 0 ? xx.y : 0.f);
-#endif
   }
 }
 
 __global__ void ReluGradientKernelHalf(
-    const int N, const half* Y, const half* dY, half* dX) {
-  const half kZero = __float2half(0.0);
+    const int N, const __half* Y, const __half* dY, __half* dX) {
+  const __half kZero = __float2half(0.0);
   HIP_1D_KERNEL_LOOP(i, N) {
-#if __CUDA_ARCH__ >= 530
     dX[i] = __hgt(Y[i], kZero) ? dY[i] : kZero;
-#else
-    dX[i] = (__half2float(Y[i]) > 0) ? dY[i] : kZero;
-#endif
   }
 }
 }  // namespace
@@ -68,13 +52,13 @@ bool ReluOp<float16, HIPContext>::RunOnDevice() {
   Y->ResizeLike(X);
   if (X.size() % 2 == 0) {
     hipLaunchKernelGGL((ReluKernelHalf2), dim3(CAFFE_GET_BLOCKS(X.size() / 2)), dim3(CAFFE_HIP_NUM_THREADS), 0, context_.hip_stream(),
-        X.size() / 2, reinterpret_cast<const half2*>(X.data<float16>()),
-        reinterpret_cast<half2*>(Y->mutable_data<float16>()));
+        X.size() / 2, reinterpret_cast<const __half2*>(X.data<float16>()),
+        reinterpret_cast<__half2*>(Y->mutable_data<float16>()));
     return true;
   } else {
     hipLaunchKernelGGL((ReluKernelHalf), dim3(CAFFE_GET_BLOCKS(X.size())), dim3(CAFFE_HIP_NUM_THREADS), 0, context_.hip_stream(),
-        X.size(), reinterpret_cast<const half*>(X.data<float16>()),
-        reinterpret_cast<half*>(Y->mutable_data<float16>()));
+        X.size(), reinterpret_cast<const __half*>(X.data<float16>()),
+        reinterpret_cast<__half*>(Y->mutable_data<float16>()));
     return true;
   }
 }
@@ -88,9 +72,9 @@ bool ReluGradientOp<float16, HIPContext>::RunOnDevice() {
   CAFFE_ENFORCE_EQ(dY.size(), Y.size());
   dX->ResizeLike(Y);
   hipLaunchKernelGGL((ReluGradientKernelHalf), dim3(CAFFE_GET_BLOCKS(Y.size())), dim3(CAFFE_HIP_NUM_THREADS), 0, context_.hip_stream(),
-      Y.size(), reinterpret_cast<const half*>(Y.data<float16>()),
-      reinterpret_cast<const half*>(dY.data<float16>()),
-      reinterpret_cast<half*>(dX->mutable_data<float16>()));
+      Y.size(), reinterpret_cast<const __half*>(Y.data<float16>()),
+      reinterpret_cast<const __half*>(dY.data<float16>()),
+      reinterpret_cast<__half*>(dX->mutable_data<float16>()));
   return true;
 }
 
@@ -101,4 +85,3 @@ REGISTER_HIP_OPERATOR(ReluFp16, ReluOp<float16, HIPContext>);
 REGISTER_HIP_OPERATOR(ReluFp16Gradient, ReluGradientOp<float16, HIPContext>);
 }  // namespace caffe2
 
-#endif  // CAFFE_HAS_CUDA_FP16
