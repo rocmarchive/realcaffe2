85ac72301094a14f7c13e90944f24ad9acd9486e
Peter Yeh <pyeh@amd.com>
2018-03-07 14:28:30 -0800

    initial implementation of miopen relu

--- caffe2/operators/relu_op_miopen.cc
+++ caffe2/operators/relu_op_miopen.cc
@@ -14,29 +14,30 @@
  * limitations under the License.
  */
 
-#include "caffe2/core/context_gpu.h"
-#include "caffe2/core/cudnn_wrappers.h"
+#include "caffe2/core/context_hip.h"
+#include "caffe2/core/miopen_wrapper.h"
 #include "caffe2/core/operator.h"
 #include "caffe2/core/types.h"
 
 namespace caffe2 {
 
-class CuDNNReluOp final : public Operator<CUDAContext> {
+class MiOPENReluOp final : public Operator<HIPContext> {
  public:
-  CuDNNReluOp(const OperatorDef& operator_def, Workspace* ws)
-      : Operator<CUDAContext>(operator_def, ws),
-        cudnn_wrapper_(&context_),
-        order_(StringToStorageOrder(
-            OperatorBase::GetSingleArgument<string>("order", "NCHW"))) {
-    CUDNN_ENFORCE(cudnnCreateTensorDescriptor(&data_desc_));
-    CUDNN_ENFORCE(cudnnCreateActivationDescriptor(&activ_desc_));
-    CUDNN_ENFORCE(cudnnSetActivationDescriptor(
-        activ_desc_, CUDNN_ACTIVATION_RELU, CUDNN_PROPAGATE_NAN, 0.0));
+  MiOPENReluOp(const OperatorDef& operator_def, Workspace* ws)
+      : Operator<HIPContext>(operator_def, ws),
+        miopen_wrapper_(&context_),
+        alpha_(OperatorBase::GetSingleArgument<double>("alpha", 1.0)),
+        beta_(OperatorBase::GetSingleArgument<double>("beta", 0.0)),
+        power_(OperatorBase::GetSingleArgument<double>("power", 1.0)) {
+    MIOPEN_ENFORCE(miopenCreateTensorDescriptor(&data_desc_));
+    MIOPEN_ENFORCE(miopenCreateActivationDescriptor(&activ_desc_));
+    MIOPEN_ENFORCE(miopenSetActivationDescriptor(
+        activ_desc_, MIOPEN_ACTIVATION_RELU, alpha_, beta_, power_));
   }
 
-  ~CuDNNReluOp() {
-    CUDNN_ENFORCE(cudnnDestroyTensorDescriptor(data_desc_));
-    CUDNN_ENFORCE(cudnnDestroyActivationDescriptor(activ_desc_));
+  ~MiOPENReluOp() {
+    MIOPEN_ENFORCE(miopenDestroyTensorDescriptor(data_desc_));
+    MIOPEN_ENFORCE(miopenDestroyActivationDescriptor(activ_desc_));
   }
 
   template <typename T>
@@ -51,36 +52,35 @@ class CuDNNReluOp final : public Operator<CUDAContext> {
     }
 
     // See if we need to reshape.
-    if (X.dims() != cudnn_input_dims_) {
+    if (X.dims() != miopen_input_dims_) {
       VLOG(1) << "Setting descriptors.";
-      cudnn_input_dims_ = X.dims();
+      miopen_input_dims_ = X.dims();
       int C = 1, H = 1, W = 1;
       if (X.ndim() == 4) {
         // Normal 4-dimensional tensors for images.
-        C = (order_ == StorageOrder::NCHW ? X.dim32(1) : X.dim32(3));
-        H = (order_ == StorageOrder::NCHW ? X.dim32(2) : X.dim32(1));
-        W = (order_ == StorageOrder::NCHW ? X.dim32(3) : X.dim32(2));
+        C = X.dim32(1);
+        H = X.dim32(2);
+        W = X.dim32(3);
       } else {
         // If X is not 4-dimensional, we will simply use H = 1 and W = 1
         // and wrap everything into C.
         C = X.size() / X.dim32(0);
       }
-      CUDNN_ENFORCE(cudnnSetTensor4dDescriptor(
+      MIOPEN_ENFORCE(miopenSet4dTensorDescriptor(
           data_desc_,
-          GetCudnnTensorFormat(order_),
-          cudnnTypeWrapper<T>::type,
+          miopenTypeWrapper<T>::type,
           X.dim32(0),
           C,
           H,
           W));
     }
-    CUDNN_ENFORCE(cudnnActivationForward(
-        cudnn_wrapper_.inline_cudnn_handle(),
+    MIOPEN_ENFORCE(miopenActivationForward(
+        miopen_wrapper_.inline_miopen_handle(),
         activ_desc_,
-        cudnnTypeWrapper<T>::kOne(),
+        &alpha_,
         data_desc_,
         X.template data<T>(),
-        cudnnTypeWrapper<T>::kZero(),
+        &beta_,
         data_desc_,
         Y->template mutable_data<T>()));
     return true;
@@ -103,36 +103,40 @@ class CuDNNReluOp final : public Operator<CUDAContext> {
   }
 
  protected:
-  CuDNNWrapper cudnn_wrapper_;
-  cudnnTensorDescriptor_t data_desc_;
-  cudnnActivationDescriptor_t activ_desc_;
-  vector<TIndex> cudnn_input_dims_;
-  StorageOrder order_;
+  MiOPENWrapper miopen_wrapper_;
+  miopenTensorDescriptor_t data_desc_;
+  miopenActivationDescriptor_t activ_desc_;
+  vector<TIndex> miopen_input_dims_;
+
+  const double alpha_;
+  const double beta_;
+  const double power_;
 };
 
 
-// Note: You can see that in CuDNNReluGradientOp, we abused the cudnn interface
+// Note: You can see that in MiOPENReluGradientOp, we abused the miopen interface
 // by passing in the output tensor for both bottom and top. This is dependent on
 // the assumption that the Relu gradient actually does not rely on the bottom
 // data, or it treats input=0 the same way as input<0. This is of course not
 // very safe, but we have been running in this way in Caffe for a while so it
 // *might* be safe to assume so.
-class CuDNNReluGradientOp final : public Operator<CUDAContext> {
+class MiOPENReluGradientOp final : public Operator<HIPContext> {
  public:
-  CuDNNReluGradientOp(const OperatorDef& operator_def, Workspace* ws)
-      : Operator<CUDAContext>(operator_def, ws),
-        cudnn_wrapper_(&context_),
-        order_(StringToStorageOrder(
-            OperatorBase::GetSingleArgument<string>("order", "NCHW"))) {
-    CUDNN_ENFORCE(cudnnCreateTensorDescriptor(&data_desc_));
-    CUDNN_ENFORCE(cudnnCreateActivationDescriptor(&activ_desc_));
-    CUDNN_ENFORCE(cudnnSetActivationDescriptor(
-        activ_desc_, CUDNN_ACTIVATION_RELU, CUDNN_PROPAGATE_NAN, 0.0));
+  MiOPENReluGradientOp(const OperatorDef& operator_def, Workspace* ws)
+      : Operator<HIPContext>(operator_def, ws),
+        miopen_wrapper_(&context_),
+        alpha_(OperatorBase::GetSingleArgument<double>("alpha", 1.0)),
+        beta_(OperatorBase::GetSingleArgument<double>("beta", 0.0)),
+        power_(OperatorBase::GetSingleArgument<double>("power", 1.0)) {
+    MIOPEN_ENFORCE(miopenCreateTensorDescriptor(&data_desc_));
+    MIOPEN_ENFORCE(miopenCreateActivationDescriptor(&activ_desc_));
+    MIOPEN_ENFORCE(miopenSetActivationDescriptor(
+            activ_desc_, MIOPEN_ACTIVATION_RELU, alpha_, beta_, power_));
   }
 
-  ~CuDNNReluGradientOp() {
-    CUDNN_ENFORCE(cudnnDestroyTensorDescriptor(data_desc_));
-    CUDNN_ENFORCE(cudnnDestroyActivationDescriptor(activ_desc_));
+  ~MiOPENReluGradientOp() {
+    MIOPEN_ENFORCE(miopenDestroyTensorDescriptor(data_desc_));
+    MIOPEN_ENFORCE(miopenDestroyActivationDescriptor(activ_desc_));
   }
 
   template <typename T>
@@ -148,33 +152,32 @@ class CuDNNReluGradientOp final : public Operator<CUDAContext> {
     }
 
     // See if we need to reshape.
-    if (Y.dims() != cudnn_input_dims_) {
+    if (Y.dims() != miopen_input_dims_) {
       VLOG(1) << "Setting descriptors.";
-      cudnn_input_dims_ = Y.dims();
+      miopen_input_dims_ = Y.dims();
       int C = 1, H = 1, W = 1;
       if (Y.ndim() == 4) {
         // Normal 4-dimensional tensors for images.
-        C = (order_ == StorageOrder::NCHW ? Y.dim32(1) : Y.dim32(3));
-        H = (order_ == StorageOrder::NCHW ? Y.dim32(2) : Y.dim32(1));
-        W = (order_ == StorageOrder::NCHW ? Y.dim32(3) : Y.dim32(2));
+        C = Y.dim32(1);
+        H = Y.dim32(2);
+        W = Y.dim32(3);
       } else {
         // If Y is not 4-dimensional, we will simply use H = 1 and W = 1
         // and wrap everything into C.
         C = Y.size() / Y.dim32(0);
       }
-      CUDNN_ENFORCE(cudnnSetTensor4dDescriptor(
+      MIOPEN_ENFORCE(miopenSet4dTensorDescriptor(
           data_desc_,
-          GetCudnnTensorFormat(order_),
-          cudnnTypeWrapper<T>::type,
+          miopenTypeWrapper<T>::type,
           Y.dim32(0),
           C,
           H,
           W));
     }
-    CUDNN_ENFORCE(cudnnActivationBackward(
-        cudnn_wrapper_.inline_cudnn_handle(),
+    MIOPEN_ENFORCE(miopenActivationBackward(
+        miopen_wrapper_.inline_miopen_handle(),
         activ_desc_,
-        cudnnTypeWrapper<T>::kOne(),
+        &alpha_,
         data_desc_,
         Y.template data<T>(),
         data_desc_,
@@ -187,7 +190,7 @@ class CuDNNReluGradientOp final : public Operator<CUDAContext> {
         // ensure this is correct, a unit test is provided at
         // caffe2/python/operator_test/relu_op_test.py
         Y.template data<T>(),
-        cudnnTypeWrapper<T>::kZero(),
+        &beta_,
         data_desc_,
         dX->template mutable_data<T>()));
     return true;
@@ -209,16 +212,18 @@ class CuDNNReluGradientOp final : public Operator<CUDAContext> {
   }
 
  protected:
-  CuDNNWrapper cudnn_wrapper_;
-  cudnnTensorDescriptor_t data_desc_;
-  cudnnActivationDescriptor_t activ_desc_;
-  vector<TIndex> cudnn_input_dims_;
-  StorageOrder order_;
+  MiOPENWrapper miopen_wrapper_;
+  miopenTensorDescriptor_t data_desc_;
+  miopenActivationDescriptor_t activ_desc_;
+  vector<TIndex> miopen_input_dims_;
+  const double alpha_;
+  const double beta_;
+  const double power_;
   // Input: Y, dY; Output: dX
 };
 
 namespace {
-REGISTER_CUDNN_OPERATOR(Relu, CuDNNReluOp);
-REGISTER_CUDNN_OPERATOR(ReluGradient, CuDNNReluGradientOp);
+REGISTER_MIOPEN_OPERATOR(Relu, MiOPENReluOp);
+REGISTER_MIOPEN_OPERATOR(ReluGradient, MiOPENReluGradientOp);
 }  // namespace
 }  // namespace caffe2
