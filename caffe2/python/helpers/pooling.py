6204ceb3159b108162c51164068b0d3f1c3878ca
Rohith <rohith.nallamaddi@gmail.com>
2018-03-20 18:42:04 -0700

    added gpu_engine/miopen support

--- caffe2/python/helpers/pooling.py
+++ caffe2/python/helpers/pooling.py
@@ -22,19 +22,20 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from caffe2.python.workspace import has_hip
 
-def max_pool(model, blob_in, blob_out, use_cudnn=False, order="NCHW", **kwargs):
+def max_pool(model, blob_in, blob_out, use_gpu_engine=False, order="NCHW", **kwargs):
     """Max pooling"""
-    if use_cudnn:
-        kwargs['engine'] = 'CUDNN'
+    if use_gpu_engine:
+        kwargs['engine'] = 'MIOPEN' if has_hip else 'CUDNN'
     return model.net.MaxPool(blob_in, blob_out, order=order, **kwargs)
 
 
-def average_pool(model, blob_in, blob_out, use_cudnn=False, order="NCHW",
+def average_pool(model, blob_in, blob_out, use_gpu_engine=False, order="NCHW",
                  **kwargs):
     """Average pooling"""
-    if use_cudnn:
-        kwargs['engine'] = 'CUDNN'
+    if use_gpu_engine:
+        kwargs['engine'] = 'MIOPEN' if has_hip else 'CUDNN'
     return model.net.AveragePool(
         blob_in,
         blob_out,
