5bf95d260b9806b2e5c17931ea43f7738a912986
P. Yeh <pyeh@amd.com>
2018-03-23 14:27:32 -0700

    memory access fix and refactoring

--- caffe2/operators/conv_op_miopen.cc
+++ caffe2/operators/conv_op_miopen.cc
@@ -26,9 +26,9 @@ namespace caffe2 {
 // something very beneficial to more recent CNN models.
     static constexpr size_t kCONV_MIOPEN_WORKSPACE_LIMIT_BYTES = 64 * 1024 * 1024;
 
-    class MiopenConvOpBase : public ConvPoolOpBase<HIPContext> {
+    class MIOPENConvOpBase : public ConvPoolOpBase<HIPContext> {
     public:
-        MiopenConvOpBase(const OperatorDef &operator_def, Workspace *ws)
+        MIOPENConvOpBase(const OperatorDef &operator_def, Workspace *ws)
                 : ConvPoolOpBase<HIPContext>(operator_def, ws),
                   miopen_wrapper_(&context_),
                   miopen_ws_nbytes_limit_(OperatorBase::GetSingleArgument<size_t>("ws_nbytes_limit",
@@ -43,9 +43,21 @@ namespace caffe2 {
             MIOPEN_ENFORCE(miopenCreateTensorDescriptor(&top_desc_for_bias_));
             MIOPEN_ENFORCE(miopenCreateConvolutionDescriptor(&conv_desc_));
             mode_ = miopenConvolution;
+
+            MIOPEN_ENFORCE(miopenInitConvolutionDescriptor(
+                    conv_desc_,
+                    mode_,
+                    pad_t(),
+                    pad_l(),
+                    stride_h(),
+                    stride_w(),
+                    dilation_h(),
+                    dilation_w()));
+
+
         }
 
-        ~MiopenConvOpBase() {
+        ~MIOPENConvOpBase() {
             MIOPEN_ENFORCE(miopenDestroyTensorDescriptor(bottom_desc_));
             MIOPEN_ENFORCE(miopenDestroyTensorDescriptor(bias_desc_));
             MIOPEN_ENFORCE(miopenDestroyTensorDescriptor(weight_desc_));
@@ -71,15 +83,27 @@ namespace caffe2 {
     };
 
 
-    class MiopenConvOp final : public MiopenConvOpBase {
+    class MIOPENConvOp final : public MIOPENConvOpBase {
     public:
-        MiopenConvOp(const OperatorDef &operator_def, Workspace *ws)
-                : MiopenConvOpBase(operator_def, ws),
+        MIOPENConvOp(const OperatorDef &operator_def, Workspace *ws)
+                : MIOPENConvOpBase(operator_def, ws),
                   requestAlgoCount_(OperatorBase::GetSingleArgument<int>("requestAlgoCount_", 1)),
                   returnedAlgoCount_(OperatorBase::GetSingleArgument<int>("returnedAlgoCount_", 1))
-        {}
+        {
 
-        ~MiopenConvOp() {}
+            MIOPEN_ENFORCE(miopenConvolutionForwardGetWorkSpaceSize(miopen_wrapper_.inline_miopen_handle(),
+                                                                    weight_desc_,
+                                                                    bottom_desc_,
+                                                                    conv_desc_,
+                                                                    top_desc_,
+                                                                    &fwdConvWsSize_));
+            hipFree(fwdConvWs);
+            HIP_CHECK(hipMalloc(&fwdConvWs, fwdConvWsSize_));
+        }
+
+        ~MIOPENConvOp() {
+            hipFree(fwdConvWs);
+        }
 
         template<typename T_X, typename T_W, typename T_B, typename MATH, typename T_Y>
         bool DoRunWithType();
@@ -88,6 +112,7 @@ namespace caffe2 {
     private:
         const int requestAlgoCount_;
         int returnedAlgoCount_;
+        char* fwdConvWs;
         size_t fwdConvWsSize_;
         miopenConvAlgoPerf_t perf_;
         // Input: X, W, b
@@ -95,19 +120,41 @@ namespace caffe2 {
         INPUT_TAGS(INPUT, FILTER, BIAS);
     };
 
-    class MiopenConvGradientOp final : public MiopenConvOpBase {
+    class MIOPENConvGradientOp final : public MIOPENConvOpBase {
     public:
-        MiopenConvGradientOp(const OperatorDef &operator_def, Workspace *ws)
-                : MiopenConvOpBase(operator_def, ws),
+        MIOPENConvGradientOp(const OperatorDef &operator_def, Workspace *ws)
+                : MIOPENConvOpBase(operator_def, ws),
                   no_bias_(OperatorBase::GetSingleArgument<int>("no_bias", 0)),
                   requestAlgoCount_(OperatorBase::GetSingleArgument<int>("requestAlgoCount_", 1)),
                   returnedAlgoCount_(OperatorBase::GetSingleArgument<int>("returnedAlgoCount_", 1)) {
             CAFFE_ENFORCE(
                     !(no_bias_ && OutputSize() == 3),
                     "If bias is not present, you should not have 3 grad output.");
+
+            MIOPEN_ENFORCE(miopenConvolutionBackwardDataGetWorkSpaceSize(miopen_wrapper_.inline_miopen_handle(),
+                                                                         top_desc_,
+                                                                         weight_desc_,
+                                                                         conv_desc_,
+                                                                         bottom_desc_,
+                                                                         &bwdDataWsSize_));
+            hipFree(bwdDataWs);
+            HIP_CHECK(hipMalloc(&bwdDataWs, bwdDataWsSize_));
+
+
+            MIOPEN_ENFORCE(miopenConvolutionBackwardWeightsGetWorkSpaceSize(miopen_wrapper_.inline_miopen_handle(),
+                                                                            top_desc_,
+                                                                            bottom_desc_,
+                                                                            conv_desc_,
+                                                                            weight_desc_,
+                                                                            &bwdWeightWsSize_));
+            hipFree(bwdWeightWs);
+            HIP_CHECK(hipMalloc(&bwdWeightWs, bwdWeightWsSize_));
+
         }
 
-        ~MiopenConvGradientOp() {}
+        ~MIOPENConvGradientOp() {
+            hipFree(bwdDataWs);
+        }
 
         template<typename T_X, typename T_DY, typename T_W, typename T_B,
                 typename MATH,
@@ -121,7 +168,9 @@ namespace caffe2 {
         int returnedAlgoCount_;
         miopenConvAlgoPerf_t perf_;
         size_t bwdWeightWsSize_;
-        size_t bwdDataWsSize;
+        size_t bwdDataWsSize_;
+        char* bwdWeightWs;
+        char* bwdDataWs;
         // input: X, W, dY
         // output: dW, db, and optionally dX
         INPUT_TAGS(INPUT, FILTER, OUTPUT_GRAD);
@@ -133,7 +182,7 @@ namespace caffe2 {
 ////////////////////////////////////////////////////////////////////////////////
 
     template<typename T_X, typename T_W, typename T_B, typename MATH, typename T_Y>
-    bool MiopenConvOp::DoRunWithType() {
+    bool MIOPENConvOp::DoRunWithType() {
         auto &X = Input(INPUT);
         auto &Weight = Input(FILTER);
         auto *Y = Output(0);
@@ -157,15 +206,6 @@ namespace caffe2 {
         W_out = Y->ndim() > 3 ? Y->dim32(3) : 1;
         D_out = Y->ndim() > 4 ? Y->dim32(4) : 1;
 
-        MIOPEN_ENFORCE(miopenInitConvolutionDescriptor(
-                conv_desc_,
-                mode_,
-                pad_t(),
-                pad_l(),
-                stride_h(),
-                stride_w(),
-                dilation_h(),
-                dilation_w()));
 
         MIOPEN_ENFORCE(miopenGetConvolutionForwardOutputDim(conv_desc_,
                                                             bottom_desc_,
@@ -182,14 +222,6 @@ namespace caffe2 {
                                                    H_out,
                                                    W_out));
 
-        MIOPEN_ENFORCE(miopenConvolutionForwardGetWorkSpaceSize(miopen_wrapper_.inline_miopen_handle(),
-                                                                weight_desc_,
-                                                                bottom_desc_,
-                                                                conv_desc_,
-                                                                top_desc_,
-                                                                &fwdConvWsSize_));
-
-        std::vector<char> fwdConvWs(fwdConvWsSize_);
 
         MIOPEN_ENFORCE(miopenFindConvolutionForwardAlgorithm(miopen_wrapper_.inline_miopen_handle(),
                                                              bottom_desc_,
@@ -202,7 +234,7 @@ namespace caffe2 {
                                                              requestAlgoCount_,
                                                              &returnedAlgoCount_,
                                                              &perf_,
-                                                             &fwdConvWs,// state->fwdConvWs().get(fwdConvWsSize_);
+                                                             fwdConvWs,
                                                              fwdConvWsSize_,
                                                              exhaustive_search_));
 
@@ -218,7 +250,7 @@ namespace caffe2 {
                                                 &beta_,
                                                 top_desc_,
                                                 Y->template mutable_data<T_Y>(),
-                                                &fwdConvWs,  // state->fwdConvWs().get(fwdConvWsSize_);
+                                                fwdConvWs,
                                                 fwdConvWsSize_));
 
         //BIAS
@@ -241,7 +273,7 @@ namespace caffe2 {
         return true;
     }
 //TODO : enable fp16 support.
-    bool MiopenConvOp::RunOnDevice() {
+    bool MIOPENConvOp::RunOnDevice() {
 
         if (Input(0).IsType<float>()) {
             return DoRunWithType<float,      // X
@@ -260,7 +292,7 @@ namespace caffe2 {
     template<typename T_X, typename T_DY, typename T_W, typename T_B,
             typename MATH,
             typename T_DX, typename T_DW, typename T_DB>
-    bool MiopenConvGradientOp::DoRunWithType() {
+    bool MIOPENConvGradientOp::DoRunWithType() {
         auto &X = Input(INPUT);
         auto &Weight = Input(FILTER);
         auto &dY = Input(OUTPUT_GRAD);
@@ -286,35 +318,7 @@ namespace caffe2 {
         W_out = dY.ndim() > 3 ? dY.dim32(3) : 1;
         D_out = dY.ndim() > 4 ? dY.dim32(4) : 1;
 
-        MIOPEN_ENFORCE(miopenInitConvolutionDescriptor(
-                conv_desc_,
-                mode_,
-                pad_t(),
-                pad_l(),
-                stride_h(),
-                stride_w(),
-                dilation_h(),
-                dilation_w()));
-
-        //TODO : see if we can share output dim from fwd pass.
-        MIOPEN_ENFORCE(miopenGetConvolutionForwardOutputDim(conv_desc_,
-                                                            bottom_desc_,
-                                                            weight_desc_,
-                                                            &N_out,
-                                                            &C_out,
-                                                            &H_out,
-                                                            &W_out));
-
         //////////// BWD DATA ////////////////////////////////////////
-        MIOPEN_ENFORCE(miopenConvolutionBackwardDataGetWorkSpaceSize(miopen_wrapper_.inline_miopen_handle(),
-                                                                     top_desc_,
-                                                                     weight_desc_,
-                                                                     conv_desc_,
-                                                                     bottom_desc_,
-                                                                     &bwdDataWsSize));
-
-        // TODO create inline function
-        std::vector<char> bwdDataWs(bwdDataWsSize);
 
         MIOPEN_ENFORCE(miopenFindConvolutionBackwardDataAlgorithm
                                (miopen_wrapper_.inline_miopen_handle(),
@@ -328,8 +332,8 @@ namespace caffe2 {
                                 requestAlgoCount_,
                                 &returnedAlgoCount_,
                                 &perf_,
-                                &bwdDataWs,// state->workspace().get(fwdConvWsSize_);
-                                bwdDataWsSize,
+                                bwdDataWs,// state->workspace().get(fwdConvWsSize_);
+                                bwdDataWsSize_,
                                 exhaustive_search_));
 
 
@@ -344,19 +348,11 @@ namespace caffe2 {
                                                      &beta_,
                                                      bottom_desc_,
                                                      dX->template mutable_data<T_DX>(),
-                                                     &bwdDataWs,  // state->workspace().get(bwd_conv_ws_size);
-                                                     bwdDataWsSize));
+                                                     bwdDataWs,  // state->workspace().get(bwd_conv_ws_size);
+                                                     bwdDataWsSize_));
 
 
         //////////////////////////////   BWD WEIGHT //////////////////////
-        MIOPEN_ENFORCE(miopenConvolutionBackwardWeightsGetWorkSpaceSize(miopen_wrapper_.inline_miopen_handle(),
-                                                                        top_desc_,
-                                                                        bottom_desc_,
-                                                                        conv_desc_,
-                                                                        weight_desc_,
-                                                                        &bwdWeightWsSize_));
-
-        std::vector<char> bwd_weight_workspace(bwdWeightWsSize_);
 
         MIOPEN_ENFORCE(miopenFindConvolutionBackwardWeightsAlgorithm(miopen_wrapper_.inline_miopen_handle(),
                                                                      top_desc_,
@@ -369,7 +365,7 @@ namespace caffe2 {
                                                                      requestAlgoCount_,
                                                                      &returnedAlgoCount_,
                                                                      &perf_,
-                                                                     &bwd_weight_workspace,// state->workspace().get(bwd_conv_ws_size);
+                                                                     bwdWeightWs,// state->workspace().get(bwd_conv_ws_size);
                                                                      bwdWeightWsSize_,
                                                                      exhaustive_search_));
 
@@ -384,7 +380,7 @@ namespace caffe2 {
                                                         &beta_,
                                                         weight_desc_,
                                                         dW->template mutable_data<T_DW>(),
-                                                        &bwd_weight_workspace,  // state->workspace().get(bwd_conv_ws_size);
+                                                        bwdWeightWs,  // state->workspace().get(bwd_conv_ws_size);
                                                         bwdWeightWsSize_));
 
         ////////////////////////////////////// BIAS ///////////////////////////
@@ -404,7 +400,7 @@ namespace caffe2 {
         return true;
     }
 
-    bool MiopenConvGradientOp::RunOnDevice() {
+    bool MIOPENConvGradientOp::RunOnDevice() {
         if (Input(0).IsType<float>()) {
             return DoRunWithType<float,    //  X
                     float,    // dY
@@ -420,13 +416,13 @@ namespace caffe2 {
         return true;
     }
 
-    REGISTER_MIOPEN_OPERATOR(Conv, MiopenConvOp);
-    REGISTER_MIOPEN_OPERATOR(ConvGradient, MiopenConvGradientOp);
+    REGISTER_MIOPEN_OPERATOR(Conv, MIOPENConvOp);
+    REGISTER_MIOPEN_OPERATOR(ConvGradient, MIOPENConvGradientOp);
 
-    REGISTER_MIOPEN_OPERATOR(Conv1D, MiopenConvOp);
-    REGISTER_MIOPEN_OPERATOR(Conv1DGradient, MiopenConvGradientOp);
+    REGISTER_MIOPEN_OPERATOR(Conv1D, MIOPENConvOp);
+    REGISTER_MIOPEN_OPERATOR(Conv1DGradient, MIOPENConvGradientOp);
 
-    REGISTER_MIOPEN_OPERATOR(Conv2D, MiopenConvOp);
-    REGISTER_MIOPEN_OPERATOR(Conv2DGradient, MiopenConvGradientOp);
+    REGISTER_MIOPEN_OPERATOR(Conv2D, MIOPENConvOp);
+    REGISTER_MIOPEN_OPERATOR(Conv2DGradient, MIOPENConvGradientOp);
 
 }  // namespace caffe2
