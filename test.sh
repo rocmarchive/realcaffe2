501be2050fc349142ebfe0a384a4efd86e84cf09
rohith <rohith.nallamaddi@gmail.com>
2018-04-11 10:26:59 -0700

    filtering in only tests

--- test.sh
+++ test.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
-total_test_count=$(ls | wc -l)	
-echo $total_test_count
+total_test_count=0
 passed_count=0
 failed_tests=()
+ignore_tests=("common_test","fixed_divisor_test","module_test","timer_test","math_hip_test")
 for T in $(ls); do
-    echo $T
-    ./$T
-    if [ $? -eq 0 ]; then
-    	passed_count=$((passed_count+1))    
-    else
-        failed_tests+=($T)
+    if [[ "$T" =~ "test" ]]; then
+	if [[ "${ignore_tests[*]}" =~ "$T" ]]; then
+	    echo "+++++++++++++++++++ ignored ++++++++++++++++++++++"
+            echo "$T"
+	    continue
 	fi
-    done
-    if [ $passed_count -eq $total_test_count ]; then
-        echo "All passed"
-        exit 0
-    else
-        echo "Failed tests..."
-        echo ${failed_tests[*]}
-	echo "passed_count:"
-	echo $passed_count
-	echo "total_count:"
-	echo $total_test_count
-        exit 1
+	total_test_count=$((total_test_count+1))
+        echo $T
+        ./$T
+        if [ $? -eq 0 ]; then
+    	    passed_count=$((passed_count+1))    
+        else
+	    failed_tests+=($T)
+	fi
     fi
-echo "done"
+done
+
+echo "passed_count:"
+echo $passed_count
+echo "total_count:"
+echo $total_test_count
+
+if [ $passed_count -eq $total_test_count ]; then
+    echo "All passed"
+    exit 0
+else
+    echo "Failed tests..."
+    echo ${failed_tests[*]}
+    exit 1
+fi
+
