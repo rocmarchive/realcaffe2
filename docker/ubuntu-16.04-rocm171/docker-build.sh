d0f05a7926da06a91013a8c66bea271b889cbdb3
rohith <rohith.nallamaddi@gmail.com>
2018-04-26 17:59:22 -0700

    minor edits

--- docker/ubuntu-16.04-rocm171/docker-build.sh
+++ docker/ubuntu-16.04-rocm171/docker-build.sh
@@ -7,4 +7,5 @@ if [ $? -ne 0 ]; then { echo "ERROR: failed base image build!" ; exit 1; } fi
 
 # Build the caffe2 image
 caffe2_image=caffe2-"$1"
-docker build -f ./docker/ubuntu-16.04-rocm171/caffe2/Dockerfile --build-arg base_image=${rocm_base_image} --no-cache -t ${caffe2_image} .
+# docker build -f ./docker/ubuntu-16.04-rocm171/caffe2/Dockerfile --build-arg base_image=${rocm_base_image} --no-cache -t ${caffe2_image} .
+docker build -f ./docker/ubuntu-16.04-rocm171/caffe2/Dockerfile --no-cache -t ${caffe2_image} .
